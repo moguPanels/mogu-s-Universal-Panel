92220ae10a8ecae7badaeb5de81a37df3738d28a
moguPanels <imcoolmoguu@gmail.com>
2025-04-09 16:49:53 +0800

    Update UniversalLoader.lua

--- UniversalLoader.lua
+++ UniversalLoader.lua
@@ -1,1 +1,44 @@
-https://pastebin.com/raw/XVJfrTGK
+
+-- Mogu's Universal Panel - Main Loader Script
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Mogu's Universal Panel",
+   LoadingTitle = "Mogu's Loader",
+   LoadingSubtitle = "Password Required",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "MoguControl",
+      FileName = "UniversalSettings"
+   },
+   Discord = {
+      Enabled = true,
+      Invite = "Hn7tvHzb44",
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "Mogu's Key System",
+      Subtitle = "Password",
+      Note = "Join the Discord server to get the key. MCPMPU is the public key.",
+      FileName = "MoguKey",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Administrator.ACC", "MCPMPU"}
+   }
+})
+
+local UniversalTab = Window:CreateTab("Mogu's Universal Panel", nil)
+
+local Games = {
+   ["J - Jailbreak"] = "https://pastebin.com/raw/RpMyNU3z" -- Replace this with actual Pastebin link later
+}
+
+for gameName, scriptURL in pairs(Games) do
+   UniversalTab:CreateButton({
+      Name = gameName,
+      Callback = function()
+         loadstring(game:HttpGet(scriptURL))()
+      end
+   })
+end
